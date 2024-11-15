4a9a526573cafd6c4043c8e04ae7e5d7af726a37
angeleau <32789452+angeleau@users.noreply.github.com>
2024-11-15 16:16:08 +0100

    implemented proto color changing function

--- etchasketchy.js
+++ etchasketchy.js
@@ -7,16 +7,28 @@ function createGrid (gridSize) {
     const gridContainer = document.querySelector(".gridContainer"); 
     for (let i = 0; i < gridSize; i++) {
         const squareX = document.createElement("div");
+        squareX.setAttribute("class", "etchSquare");
         gridContainer.appendChild(squareX);
+        
         if (i === (gridSize-1)) {
             squareX.style.borderRight = "2px solid firebrick";
-            console.log("yahoo");
+            
         }
         for (let i = 0; i < gridSize; i++) {
             const squareY = document.createElement("div");
+            squareY.setAttribute("class", "etchSquare");
             squareX.appendChild(squareY);
         }
     }
 }
 
+function colorChange() {
+    const etchSquares = document.querySelectorAll(".etchSquare");
+    etchSquares.forEach ((etchSquare) => {
+        etchSquare.style.backgroundColor = "tomato";
+        console.log("yahoo");
+    });
+}
+
 createGrid(16);
+colorChange();
