dd3e6e5e7f0f18c6bca95021bd234b152823cc53
angeleau <32789452+angeleau@users.noreply.github.com>
2024-11-15 16:03:57 +0100

    create createGrid function, avoid double border issue

--- etchasketchy.js
+++ etchasketchy.js
@@ -1,1 +1,22 @@
+const gridSize = 16;
+const squareSize = 100;
 
+
+
+function createGrid (gridSize) {  
+    const gridContainer = document.querySelector(".gridContainer"); 
+    for (let i = 0; i < gridSize; i++) {
+        const squareX = document.createElement("div");
+        gridContainer.appendChild(squareX);
+        if (i === (gridSize-1)) {
+            squareX.style.borderRight = "2px solid firebrick";
+            console.log("yahoo");
+        }
+        for (let i = 0; i < gridSize; i++) {
+            const squareY = document.createElement("div");
+            squareX.appendChild(squareY);
+        }
+    }
+}
+
+createGrid(16);
